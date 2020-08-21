aaa5213a3eb9aee0f2e94f3e17f284062e4b2015
alikurtulush92 <kopanarya92@gmail.com>
2020-08-21 22:47:08 +0100

    added movement for enemy

--- js/Enemy.js
+++ js/Enemy.js
@@ -1,5 +1,18 @@
 class Enemy{
-    constructor(xPos, yPos){
+    constructor(xPos, yPos,health){
+        this.xPos = xPos
+        this.yPos = yPos
+    }
+     // When Spaceship moves left
+     movesLeft(){
+  
+    }
+    // When Spaceship moves right
+    movesRight(){
+
+    }
+     // When Spaceship moves down
+     movesDown(){
 
     }
 }
